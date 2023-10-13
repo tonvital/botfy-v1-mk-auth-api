aba908cb26a98109bdaa177f3161dad3620e5209
Ton <tonvitalferreira@gmail.com>
2023-10-13 10:37:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,30 @@
 clear
+curl --version | grep "curl"
+if [ $? -eq 0 ]
+then
+  echo "Curl já instalado."
+else
+  echo "Instalando Curl..."
+  sudo apt install curl 
+  echo "Instalação concluida do Curl!"
+fi
+
+clear
 node -v | grep "v16"
 if [ $? -eq 0 ]
 then
   echo "NodeJS V16 já instalado."
 else
   echo "Instalando NodeJS..."
-  sudo apt-get update
-  sudo apt-get install -y ca-certificates curl gnupg
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-  NODE_MAJOR=16
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-  sudo apt-get update -y
-  sudo apt-get install nodejs -y
-  echo "Terminado node js!"
+  touch ~/.profile   
+  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  nvm install 16.20.2
+  echo "Instalação concluida do NodeJS!"
 fi
 
+clear
 pm2 -v
 if [ $? -eq 0 ]
 then
@@ -27,19 +36,22 @@ else
   pm2 set pm2-logrotate:max_size 50M
   pm2 set pm2-logrotate:retain 10
   pm2 set pm2-logrotate:compress true
-  echo "Terminado pm2!"
+  echo "Instalação concluida do PM2!"
 fi
 
+clear
 echo "Baixando nova API..."
 rm -rf botfy-v1-mk-auth-api-main/
 rm -rf botfy-v1-mk-auth-api-main
 rm -rf botfy-v1-mk-auth-api
 rm -rf botfy-v1-mk-auth-api.zip
 wget --no-check-certificate -O botfy-v1-mk-auth-api.zip https://github.com/tonvital/botfy-v1-mk-auth-api/archive/main.zip && unzip -o botfy-v1-mk-auth-api.zip
+
 clear
-cd botfy-v1-mk-auth-api-main
 echo "Instalando API..."
+cd botfy-v1-mk-auth-api-main
 npm install
+
 clear
 pm2 stop api
 pm2 delete api
