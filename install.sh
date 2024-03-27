3c25eef422072798564c18cf0fa810dd8710a0f4
Ton <tonvitalferreira@gmail.com>
2024-03-27 16:47:42 -0300

    last update

--- install.sh
+++ install.sh
@@ -17,8 +17,7 @@ then
   echo "NodeJS V16 já instalado."
 else
   echo "Instalando NodeJS..."
-  touch ~/.profile   
-  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
