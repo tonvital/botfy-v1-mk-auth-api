5b2db52d0cddfd40ab3dd5a47280a9cae612edc4
Washington Vital Ferreira <50684920+tonvital@users.noreply.github.com>
2025-04-07 20:31:17 -0300

    last update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [ "$EUID" -ne 0 ]; then
   echo "🚫 Este script precisa ser executado como root (use: sudo ./install.sh)"
   exit 1
@@ -47,38 +48,38 @@ else
   echo "Instalação concluida do PM2!"
 fi
 
-# clear
-# echo "Baixando nova API..."
-# rm -rf botfy-v1-mk-auth-api-main/
-# rm -rf botfy-v1-mk-auth-api-main
-# rm -rf botfy-v1-mk-auth-api
-# rm -rf botfy-v1-mk-auth-api.zip
-# wget --no-check-certificate -O botfy-v1-mk-auth-api.zip https://github.com/tonvital/botfy-v1-mk-auth-api/archive/main.zip && unzip -o botfy-v1-mk-auth-api.zip
+clear
+echo "Baixando nova API..."
+rm -rf botfy-v1-mk-auth-api-main/
+rm -rf botfy-v1-mk-auth-api-main
+rm -rf botfy-v1-mk-auth-api
+rm -rf botfy-v1-mk-auth-api.zip
+wget --no-check-certificate -O botfy-v1-mk-auth-api.zip https://github.com/tonvital/botfy-v1-mk-auth-api/archive/main.zip && unzip -o botfy-v1-mk-auth-api.zip
 
-# clear
-# echo "Instalando API..."
-# cd botfy-v1-mk-auth-api-main
-# npm install
+clear
+echo "Instalando API..."
+cd botfy-v1-mk-auth-api-main
+npm install
 
-# clear
-# pm2 stop api
-# pm2 delete api
-# pm2 start pm2-run.json --exp-backoff-restart-delay=100
-# pm2 save
-# pm2 startup
-# pm2 save
+clear
+pm2 stop api
+pm2 delete api
+pm2 start pm2-run.json --exp-backoff-restart-delay=100
+pm2 save
+pm2 startup
+pm2 save
 
-# clear
-# echo "Checando API..."
-# sleep 5
+clear
+echo "Checando API..."
+sleep 5
 
-# curl -Is http://localhost:9657/botfy-v1-mk-auth?f=heathCheck | grep "200"
+curl -Is http://localhost:9657/botfy-v1-mk-auth?f=heathCheck | grep "200"
 
-# if [ $? -eq 0 ]
-# then
-# echo "API botfy-mk-auth foi instalada com sucesso!"
-# else
-# echo "Oops! Algo deu errado na instalação!!!"
-# fi
+if [ $? -eq 0 ]
+then
+echo "API botfy-mk-auth foi instalada com sucesso!"
+else
+echo "Oops! Algo deu errado na instalação!!!"
+fi
 
-# pm2 status
+pm2 status
